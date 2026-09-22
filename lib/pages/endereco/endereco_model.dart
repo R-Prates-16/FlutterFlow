3f49e06f3cc736fb006ca061e4467bff4cc7e174
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-09-22 23:15:32 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/endereco/endereco_model.dart
+++ lib/pages/endereco/endereco_model.dart
@@ -1,16 +1,8 @@
 import '/backend/api_requests/api_calls.dart';
-import '/flutter_flow/ff_builtin_enums.dart';
-import '/flutter_flow/flutter_flow_icon_button.dart';
-import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
-import '/flutter_flow/flutter_flow_widgets.dart';
-import 'dart:ui';
 import '/index.dart';
 import 'endereco_widget.dart' show EnderecoWidget;
-import 'package:easy_debounce/easy_debounce.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
 
 class EnderecoModel extends FlutterFlowModel<EnderecoWidget> {
   ///  State fields for stateful widgets in this page.
